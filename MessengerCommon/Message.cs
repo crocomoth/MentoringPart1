4646ad60207f58438b7ff3be387bbc325fa9fb9b
crocomoth <justletmeplaymf@gmail.com>
2019-03-12 20:42:51 +0300

    Initial commit

---
+++ MessengerCommon/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerCommon
+{
+    public class Message
+    {
+        public Message()
+        {
+        }
+
+        public Message(string author, string text)
+        {
+            Author = author ?? throw new ArgumentNullException(nameof(author));
+            Text = text ?? throw new ArgumentNullException(nameof(text));
+        }
+
+        public string Author { get; set; }
+
+        public string Text { get; set; }
+    }
+}
